f661b75edfbdd0bb765b4fdcc6b5cfa8447daaf9
IvelisBecker <ivelisbecker@gmail.com>
2025-03-05 16:17:57 -0600

    Fixed details between schema and seeds.sql

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,7 +1,10 @@
-DROP DATABASE IF EXISTS
-employees_db;
+-- Drop the database if it exists
+DROP DATABASE IF EXISTS employees_db;
+
+-- Create a new database
 CREATE DATABASE employees_db;
 
+-- Connect to the newly created database
 \c employees_db;
 
 -- Creating departments table
@@ -10,25 +13,25 @@ CREATE TABLE departments (
     department VARCHAR(30) NOT NULL
 );
 
--- Creating role table
+-- Creating roles table
 CREATE TABLE roles (
     role_id SERIAL PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
     department_id INTEGER NOT NULL,
     FOREIGN KEY (department_id)
-    REFERENCES departments
+    REFERENCES departments(department_id)
     ON DELETE SET NULL
 );
 
--- Creating employee table
+-- Creating employees table
 CREATE TABLE employees (
     employee_id INTEGER PRIMARY KEY,
     employee_first_name VARCHAR(30),
     employee_last_name VARCHAR(30),
     role_id INTEGER NOT NULL,
     FOREIGN KEY (role_id)
-    REFERENCES roles
+    REFERENCES roles(role_id)
     ON DELETE SET NULL,
     manager_id INTEGER
-);
+)
